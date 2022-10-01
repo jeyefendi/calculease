0e4565ccc806d5b86cb4663d025cd4654d1c4898
jeyefendi <jeyhunfarajov@gmail.com>
2022-10-02 02:17:12 +0300

    completed desktop and tablet css

--- src/LeaseCalculator.tsx
+++ src/LeaseCalculator.tsx
@@ -5,42 +5,57 @@ export default function LeaseCalculator() {
     <div className="lease-calculator-container">
       <h1>Рассчитайте стоимость автомобиля в лизинг</h1>
       <form className="lease-calculator-form">
-        <div className="form-item">
-          <label htmlFor="lease-amount">Стоимость автомобиля</label>
-          <input
-            type="number"
-            name="lease-amount"
-            placeholder="0"
-            className="form-input"
-          />
+        <div className="lease-form-row1">
+          
+          <div className="form-item">
+            <label htmlFor="lease-amount">Стоимость автомобиля</label>
+            <input
+              type="number"
+              name="lease-amount"
+              placeholder="3 300 000"
+              className="form-input"
+            />
+          </div>
+
+          <div className="form-item">
+            <label htmlFor="initial-payment">Первоначальный взнос</label>
+            <input
+              type="number"
+              name="initial-payment"
+              placeholder="420 000"
+              className="form-input"
+            />
+          </div>
+
+          <div className="form-item">
+            <label htmlFor="lease-term">Срок лизинга</label>
+            <input
+              type="number"
+              name="lease-term"
+              placeholder="60"
+              className="form-input"
+            />
+          </div>
         </div>
 
-        <div className="form-item">
-          <label htmlFor="initial-payment">Первоначальный взнос</label>
-          <input
-            type="number"
-            name="initial-payment"
-            placeholder="0"
-            className="form-input"
-          />
-        </div>
+        <div className="lease-form-row2">
+          <div className="form-item">
+            <label>Сумма договора лизинга</label>
+            <h2>4 467 313 Р</h2>
+          </div>
 
-        <div className="form-item">
-          <label htmlFor="lease-term">Срок лизинга</label>
-          <input
-            type="number"
-            name="lease-term"
-            placeholder="0"
-            className="form-input"
-          />
-        </div>
+          <div className="form-item">
+            <label>Ежемесячный платеж от</label>
+            <h2>114 455 Р</h2>
+          </div>
 
-        <div className="form-action">
-          <input
-            type="submit"
-            value="Оставить заявку"
-            className="form-button"
-          />
+          <div className="form-action">
+            <input
+              type="submit"
+              value="Оставить заявку"
+              className="form-button"
+            />
+          </div>
         </div>
       </form>
     </div>
